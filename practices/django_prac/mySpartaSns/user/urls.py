3e2eb46f426d8bb314906e03d9183994a021c53c
dana <bear9473@gmail.com>
2023-04-18 16:26:20 +0900

    sparta django_basic of SNS

---
+++ practices/django_prac/mySpartaSns/user/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('sign-up/', views.sign_up_view, name='sign-up'),
+    path('sign-in/', views.sign_in_view, name='sign-in'),
+    path('logout/', views.logout, name='logout'),
+    path('user/', views.user_view, name='user-list'),
+    path('user/follow/<int:id>/', views.user_follow, name='user-follow'),
+
+]
---
+++ practices/django_prac/mySpartaSns/user/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+
+def sign_up_view(request):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2 = request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:  # 입력받은 것과 데이터이름이 같으면 warning message
+                return redirect('/sign-up')
+            else:  # 일치하는 것이 없으면 가입 완료!
+                UserModel.objects.create(
+                    username=username, password=password, bio=bio)
+                return redirect('/sign-in')
+
+
+@csrf_exempt
+def sign_in_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = auth.authenticate(request, username=username, password=password)
+
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
+        else:
+            return redirect('/sign-in')
+
+    elif request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+# user/views.py 
+# 
+
+@login_required
+def user_view(request):
+    if request.method == 'GET':
+        # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
+        user_list = UserModel.objects.all().exclude(username=request.user.username)
+        return render(request, 'user/user_list.html', {'user_list': user_list})
+
+
+@login_required
+def user_follow(request, id):
+    me = request.user
+    click_user = UserModel.objects.get(id=id)
+    if me in click_user.followee.all():
+        click_user.followee.remove(request.user)
+    else:
+        click_user.followee.add(request.user)
+    return redirect('/user')
